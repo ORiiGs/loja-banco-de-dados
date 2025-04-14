bea348e805506dd7cf94b183e15858ff6f26bb41
ORiiGs <102189756+ORiiGs@users.noreply.github.com>
2025-04-13 23:16:21 -0300

    Update init-mongo.js

--- scripts/init-mongo.js
+++ scripts/init-mongo.js
@@ -1,9 +1,8 @@
 const DB_NAME = process.env.MONGODB_LOJA_DATABASE;
 
-db = db.getSiblingDB('tarefas');
+db = db.getSiblingDB('loja');
 
 db.produtos.insertMany([
-  { nome: "Tarefa 1", Qt_Questoes: 12, dificuldade: "3/5" },
-  { nome: "Tarefa 2", Qt_Questoes: 7, dificuldade: "2/5" },
-  { nome: "Tarefa 3", Qt_Questoes: 18, dificuldade: "5/5" },
+  { nome: "Camisa", Preço: 80.00 },
+  { nome: "Calça", Preço: 160.00 },
 ]);
